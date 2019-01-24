245f84b0311df7e1f7aa5f28012ff86f31d01f8a
Pdkary <pdkary@gmail.com>
2019-01-23 22:21:30 -0500

    initial commit

---
+++ src/main/java/org/pdkary/dos/DosApplication.java
@@ -0,0 +1,14 @@
+package org.pdkary.dos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DosApplication.class, args);
+	}
+
+}
+
